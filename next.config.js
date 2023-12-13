cfbf54de98bd6de2e073cfa4f5ad946fd7e42360
DSC-LABEL <dogesoundclub@gmail.com>
2023-12-13 16:50:22 +0900

    changed pwa module

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // next.config.js:
-const withPWA = require('next-pwa');
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,15 +10,18 @@ const nextConfig = {
     });
     return config;
   },
+  // 여기에 기존에 있던 다른 설정들을 추가합니다.
 };
 
-module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public', // PWA 파일이 저장될 폴더
-    register: true, // 서비스 워커 등록 여부
-    skipWaiting: true, // 서비스 워커가 즉시 활성화되도록 설정
-    swSrc: 'service-worker.js', // 커스텀 Service Worker 파일 경로
-    // 필요한 경우 추가 설정을 할 수 있습니다.
-  },
-});
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require("@ducanh2912/next-pwa").default({
+      dest: "public", // PWA 파일이 저장될 폴더
+      register: true, // 서비스 워커 등록 여부
+      skipWaiting: true, // 서비스 워커가 즉시 활성화되도록 설정
+      sw: '/sw.js', // 커스텀 Service Worker 파일 경로
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
