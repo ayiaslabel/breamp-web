0f0486243d5b39d2ec5add59d1e2ef1be6d878a1
DSC-LABEL <dogesoundclub@gmail.com>
2023-12-13 16:16:40 +0900

    configured pwa settings

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+// next.config.js:
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
@@ -9,4 +12,13 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public', // PWA 파일이 저장될 폴더
+    register: true, // 서비스 워커 등록 여부
+    skipWaiting: true, // 서비스 워커가 즉시 활성화되도록 설정
+    swSrc: 'service-worker.js', // 커스텀 Service Worker 파일 경로
+    // 필요한 경우 추가 설정을 할 수 있습니다.
+  },
+});
